6aeb8d91afe88d506d932d3ee10d4b5a2d1c3269
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-03-22 18:21:09 -0400

    Keyboard logic part 1: setup addEventListener for keypress event

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import words from "./wordList.json";
 import Keyboard from "./Keyboard";
 import { HangmanWord } from "./HangmanWord";
@@ -8,11 +8,35 @@ function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
+  console.log(wordToGuess);
+
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   // console.log(wordToGuess);
   const incorrectLetters = guessedLetters.filter(
     (letter) => !wordToGuess.includes(letter)
   );
+
+  function addGuessedLetter(letter: string) {
+    if (guessedLetters.includes(letter)) return;
+    setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+  }
+
+  useEffect(() => {
+    const hanlder = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+
+    document.addEventListener("keypress", hanlder);
+
+    return () => {
+      document.removeEventListener("keypress", hanlder);
+    };
+  }, [guessedLetters]);
 
   return (
     <div
